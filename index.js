4758ddb1c2c81ff0c68550b6d127b56f54a7d7a5
ztfcode <75156853+ztfcode@users.noreply.github.com>
2022-05-31 02:14:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const puppeteer = require('puppeteer')
 const app = require('express')
+const PORT = process.env.PORT || 5000
 
-app.
+app.get('/', (req, res) => {
+  res.send(JSON.stringify({
+    status: "ok",
+    code: 200,
+    message: "already to use"
+  }, null, 2))
+})
+app.listen(PORT, () => console.log('App listen on PORT: %s', PORT))
