4c19179b6bc2f6c7b74ca4b4220dad438effe23b
ztfcode <75156853+ztfcode@users.noreply.github.com>
2022-05-31 08:57:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
-app.listen(port, () => {
-    console.log('server is listening on port 5000')
-})
-
-app.get('/api/products', (req, res) => {
+app.get('/api', (req, res) => {
     res.json([
         { name: 'iPhone', price: 800 },
         { name: 'iPad', price: 650 },
         { name: 'iWatch', price: 750 }
     ])
+})
+
+app.listen(PORT, () => {
+    console.log('server is listening on port 5000')
 })
